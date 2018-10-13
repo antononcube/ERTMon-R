902a4d44fde9fcbadc8baa2fd8f52f2a540c32dc
Anton Antonov <anton.antonov@qio.io>
2018-10-13 17:57:20 -0500

    Added missing GPL 3.0 notes.

--- R/FakeMedicalDataCreation.R
+++ R/FakeMedicalDataCreation.R
@@ -1,3 +1,26 @@
+##===========================================================
+## Event records transformations OOP fake medical data creation
+## Copyright (C) 2018  Anton Antonov
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+## Written by Anton Antonov,
+## antononcube @@@ gmail ... com,
+## Windermere, Florida, USA.
+##===========================================================
+
+
 #---
 # Title: Fake medical data creation
 # Author: Anton Antonov
