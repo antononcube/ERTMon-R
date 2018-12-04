8e8e640aa425337b32dbb3369c5d90bd938b9344
Anton Antonov <anton.antonov@qio.io>
2018-12-03 22:32:18 -0500

    Test for ERTMon pipeline through data read from a directory.

---
+++ tests/from-directory-pipeline.R
@@ -0,0 +1,40 @@
+
+if(FALSE) {
+  
+  rm(list=ls())
+  
+  dirName <- if( grepl("vignettes", getwd()) ) { file.path( getwd(), "..", "data", "FakeData") } else { file.path( getwd(), "data", "FakeData")}
+  print(dirName)
+  
+  source( file.path( dirName, "..", "..", "R", "LoadClasses.R") )
+  
+  cat("\nReading data from \"", dirName, "\" ...\n")
+  
+  if(FALSE ) {
+    ertmon1 <-
+      ERTMonUnit() %>%
+      ERTMonReadDataFromDirectory( dirName )
+  } else { 
+    ertmon1 <-
+      ERTMonUnit() %>%
+      ERTMonReadComputationSpecification( file.path( dirName, "computationSpecification.csv" ) ) %>% 
+      ERTMonReadDataFromDirectory( dirName, readCompSpecQ = FALSE )
+  }
+  
+  cat("...DONE\n")
+  
+  cat("\nMain processing function...\n")
+  
+  ertmon1 <-
+    ertmon1 %>%
+    ERTMonProcessEventRecords()
+  
+  cat("...DONE\n")
+  
+  View(ertmon1 %>% ERTMonTakeTimeCellsInterpretation)
+  
+  cat("\nContingency matrices:\n")
+  smats <- ertmon1 %>% ERTMonTakeContingencyMatrices()
+  cat(names(smats), "\n")
+  
+}
