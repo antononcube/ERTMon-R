99b9001d391ea1328774fbab69f435d147860c36
Anton Antonov <anton.antonov@qio.io>
2019-03-22 20:26:53 -0400

    Added explanations.

--- tests/testthat/test_collapsing_of_feature_matrices.R
+++ tests/testthat/test_collapsing_of_feature_matrices.R
@@ -1,16 +1,27 @@
 context("Collapsing of feature matrices")
 library(ERTMon)
 
-numberOfEntities <- 10
-nIntervals <- 5
+##===========================================================
+## The tests implemented are
+## 1) [X] sanity check tests,
+## 2) [X] correctness test for colSums,
+## 3) [X] correctness test for rowSums.
 
+## More tests are needed: 
+## 1) [ ] with that other collapse functions, 
+## 2) [ ] with specified entity ID's,
+## 3) [ ] with specifications that produce feature sub-matrices with different numbers of columns,
+## 4) [ ] appropriate warnings tests.
+##===========================================================
+
+## Generate data.
 testData <-
-  ERTMonSimpleTestData( numberOfEntities = numberOfEntities, numberOfVariables = 5, 
+  ERTMonSimpleTestData( numberOfEntities = 10, numberOfVariables = 5, 
                         timeInterval = 900, numberOfTimeCells = 40, randomStartTimesQ = TRUE, 
                         variableFunction = "Linear", 
                         exportDirectoryName = NULL )
 
-
+## Transform the generated event records.
 ertmon0 <-
   ERTMonUnit() %>%
   ERTMonSetEventRecords( testData$EventRecords ) %>%
@@ -18,21 +29,28 @@ ertmon0 <-
   ERTMonSetComputationSpecification( testData$ComputationSpecification ) %>% 
   ERTMonProcessEventRecords( alignmentSpec = "MinTime" )
 
+## Extract feature sub-matrices.
 feMats <- ertmon0 %>% ERTMonTakeContingencyMatrices
 
+## Select feature sub-matrices names to focus on.
 focusMatNames <- grep( "Mean$", ertmon0 %>% ERTMonTakeFeatureNamePrefixes, value = T )
   
+## Collapse feature specified sub-matrices with the default collapse function, colSums.
 ertmon0 <- 
   ertmon0 %>% 
   ERTMonCollapseFeatureMatrices( matrixNames = focusMatNames )
 
+## Get the result. 
 resMats1 <- ertmon0 %>% ERTMonTakeValue
 
+## Collapse feature specified sub-matrices with rowSums.
 ertmon0 <- 
   ertmon0 %>% 
   ERTMonCollapseFeatureMatrices( matrixNames = focusMatNames, collapseFunction = rowSums )
 
+## Get the result.
 resMats2 <- ertmon0 %>% ERTMonTakeValue
+
 
 test_that("Collapse feature sub-matrices with colSums (default).", {
   expect_is( ertmon0, "ERTMon" )
@@ -41,8 +59,10 @@ test_that("Collapse feature sub-matrices with colSums (default).", {
   expect_true( length(resMats1) == length(focusMatNames) )
   expect_equal( mean( purrr::map_chr( resMats1, class ) == "numeric" ), 1 )
   
+  ## This the "what is the function supposed to do" test.
   expect_equal( mean( purrr::map2_dbl( resMats1, purrr::map( feMats[focusMatNames], colSums), function(x,y) mean(x==y) ) ), 1 )
 })
+
 
 test_that("Collapse feature sub-matrices with rowSums.", {
   expect_is( ertmon0, "ERTMon" )
@@ -51,5 +71,7 @@ test_that("Collapse feature sub-matrices with rowSums.", {
   expect_true( length(resMats2) == length(focusMatNames) )
   expect_equal( mean( purrr::map_chr( resMats2, class ) == "numeric" ), 1 )
   
+  ## This the "what is the function supposed to do" test.
   expect_equal( mean( purrr::map2_dbl( resMats2, purrr::map( feMats[focusMatNames], rowSums), function(x,y) mean(x==y) ) ), 1 )
 })
+
